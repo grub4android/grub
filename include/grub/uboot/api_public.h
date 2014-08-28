d52a8a4771a613b952042de4da8c27fdcd29f824
M1cha <sigmaepsilon92@gmail.com>
2014-08-28 13:27:23 +0200

    uboot: add gunzip syscall
    and reformat some uboot files with indent

--- include/grub/uboot/api_public.h
+++ include/grub/uboot/api_public.h
@@ -100,6 +100,7 @@ enum
   API_INPUT_GETKEY,
   API_BOOT_CREATE_TAGS,
   API_BOOT_PREPARE,
+  API_TOOL_GUNZIP,
   API_MAXCALL
 };
 
@@ -188,12 +189,13 @@ struct display_info
   int color_format;
 };
 
-struct tags_info {
+struct tags_info
+{
   void *tags_addr;
   const char *cmdline;
   void *ramdisk;
   unsigned ramdisk_size;
-  void* dt;
+  void *dt;
   unsigned dt_size;
 };
 
