d73c38b1aad17a7af5eea1976b860fa578bfd80e
M1cha <sigmaepsilon92@gmail.com>
2015-01-21 19:17:45 +0100

    grub-core: commands: search: add support for searching gpt part names

---
+++ grub-core/commands/search_partname.c
@@ -0,0 +1,5 @@
+#define DO_SEARCH_PARTNAME 1
+#define FUNC_NAME grub_search_partname
+#define COMMAND_NAME "search.partname"
+#define HELP_MESSAGE N_("Search devices by partition name. If VARIABLE is specified, the first device found is set to a variable.")
+#include "search.c"
